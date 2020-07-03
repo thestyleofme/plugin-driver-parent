440c3a40798c3f27ab9a7bf574d35594c5502974
isaac <codingdebugallday@163.com>
2020-07-03 15:34:33 +0800

    :recycle: add plugin datasource filter

--- plugin-driver-main/src/main/java/com/github/codingdebugallday/driver/core/infra/utils/Preconditions.java
+++ plugin-driver-main/src/main/java/com/github/codingdebugallday/driver/core/infra/utils/Preconditions.java
@@ -3,6 +3,7 @@ package com.github.codingdebugallday.driver.core.infra.utils;
 import java.util.Objects;
 
 import com.github.codingdebugallday.driver.core.domain.entity.PluginDatasource;
+import org.springframework.util.StringUtils;
 
 /**
  * <p>
@@ -26,6 +27,23 @@ public class Preconditions {
      * @return true/false
      */
     public static boolean pluginDatasourceFilter(PluginDatasource left, PluginDatasource right) {
-        return true;
+        if (Objects.isNull(right)) {
+            return true;
+        }
+        if (!StringUtils.isEmpty(right.getPluginId()) &&
+                !right.getPluginId().equals(left.getPluginId())) {
+            return false;
+        }
+        if (!StringUtils.isEmpty(right.getDatasourceClass()) &&
+                !right.getDatasourceClass().equals(left.getDatasourceClass())) {
+            return false;
+        }
+        if (!StringUtils.isEmpty(right.getDatasourceType()) &&
+                !right.getDatasourceType().equals(left.getDatasourceType())) {
+            return false;
+        }
+        // 暂时只比较这些
+        return !Objects.nonNull(right.getEnabledFlag()) ||
+                right.getEnabledFlag().equals(left.getEnabledFlag());
     }
 }
